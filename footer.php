5a60e41aa7383e93672bdc5e32bbd7a738cebd3b
Shawn <admin@shawnzeng.com>
2024-11-11 16:11:44 +0800

    🐞修复 sitemap 地址多了一个“/”的问题

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
         <footer id="footer" class="clear-both">
             <div class="container">
-                <p>&copy; <?php echo date('Y'); ?>&nbsp;<?php $this->options->title(); ?>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?php $this->options->siteUrl(); ?>/sitemap.xml">Sitemap</a><?php if($this->options->beian){ echo '&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://beian.miit.gov.cn/" target="_blank">' . $this->options->beian . '</a>'; } ?></p>
+                <p>&copy; <?php echo date('Y'); ?>&nbsp;<?php $this->options->title(); ?>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?php $this->options->siteUrl(); ?>sitemap.xml">Sitemap</a><?php if($this->options->beian){ echo '&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://beian.miit.gov.cn/" target="_blank">' . $this->options->beian . '</a>'; } ?></p>
                 <p>Typecho theme <?php echo '<a href="https://shawnzeng.com/archives/30/" target="_blank">' . __THEME_NAME__ . '</a>&nbsp;'  . __THEME_VERSION__; ?> by <a href="https://shawnzeng.com" target="_blank">Shawn</a>. All rights reserved.</p>
             </div>
         </footer>
